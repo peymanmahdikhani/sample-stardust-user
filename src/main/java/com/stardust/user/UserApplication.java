fdd2ac2680a1d48de39a78fe46cb226216866d03
peyman mahdikhani <payman.mahdikhani@gmail.com>
2020-11-27 18:51:25 +0330

    Implement JMS ActiveMQ channels between microservices.closes #13

--- src/main/java/com/stardust/user/UserApplication.java
+++ src/main/java/com/stardust/user/UserApplication.java
@@ -2,12 +2,8 @@ package com.stardust.user;
 
 import com.stardust.StardustApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.stardust"})
 @Import(StardustApplication.class)
 public class UserApplication {
 
